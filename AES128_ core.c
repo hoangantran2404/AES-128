6c13a852e0f8d0b629082163686f24dee73290d7
Ngo Tran Hoang An <ngotranhoangan2007@gmail.com>
2025-12-11 21:48:30 -0600

    Load file AES128_core.c for running on software

---
+++ AES128_ core.c
@@ -0,0 +1,170 @@
+#include <stdint.h>
+
+void RotWord(uint32_t data_in[4], uint32_t data_out[4])
+{
+    uint32_t temp = data_in[0];
+
+    data_out[0] = data_in[1];
+    data_out[1] = data_in[2];
+    data_out[2] = data_in[3];
+    data_out[3] = temp;
+}
+void AddRoundKey(const uint32_t state[4], const uint32_t roundKey[4], uint32_t data_out[4])
+{
+    data_out[0] = state[0] ^ roundKey[0];
+    data_out[1] = state[1] ^ roundKey[1];
+    data_out[2] = state[2] ^ roundKey[2];
+    data_out[3] = state[3] ^ roundKey[3];
+}
+
+ uint8_t GFMult(uint8_t a, uint8_t b)
+{
+    uint8_t temp_a = a;
+    uint8_t      p = 0x00;
+
+    for (int i=0; i<8; i++){
+        if((b & 0x01) == 1)
+            p ^= temp_a;
+
+        uint8_t hi_bit_set = (temp_a & 0x80); 
+
+        temp_a = temp_a << 1;
+
+        if(hi_bit_set){
+            temp_a ^= 0x1b; 
+        }
+        b = b >> 1;
+    }
+
+    return p;
+}
+void MixColumns(uint32_t word_in[4])
+{
+    uint32_t temp[4];
+    uint8_t  c[4];
+    uint8_t  r[4];
+
+    for(int i = 0; i< 4; i++){
+        c[0] = ((word_in[i]) >> 0)  & 0xFF;
+        c[1] = ((word_in[i]) >> 8)  & 0xFF;
+        c[2] = ((word_in[i]) >> 16) & 0xFF;
+        c[3] = ((word_in[i]) >> 24) & 0xFF;
+    
+        r[0] = GFMult(c[0], 0x02) ^ GFMult(c[1], 0x03) ^ c[2] ^ c[3];
+        r[1] = c[0] ^ GFMult(c[1], 0x02) ^ GFMult(c[2], 0x03) ^ c[3];
+        r[2] = c[0] ^ c[1] ^ GFMult(c[2],0x02) ^ GFMult(c[3],0x03);
+        r[3] = GFMult(c[0],0x03) ^ c[1] ^ c[2] ^ GFMult(c[3],0x02);
+        
+        temp[i] = (r[0] << 0) | (r[1] << 8) | (r[2] << 16) | (r[3] << 24);
+    }
+
+    for (int i=0; i<4; i++){
+        word_in[i] = temp[i];
+    }
+}
+
+const uint8_t rcon[11] = {
+        0x00, 0x01, 0x02, 0x04, 
+        0x08, 0x10, 0x20, 0x40, 
+        0x80, 0x1b, 0x36
+};
+ 
+void ShiftRows (uint32_t state[4])
+{
+    uint8_t temp[16];
+    for (int i = 0; i < 16; i++){
+        temp[i] = (state[i/4] >> ((i%4)*8)) & 0xFF;
+    }
+
+    state[0] = (temp[0] <<0) | (temp[5] <<8) | (temp[10] <<16) | (temp[15] << 24);
+    state[1] = (temp[4] <<0) | (temp[9] <<8) | (temp[14] <<16) | (temp[3] << 24);
+    state[2] = (temp[8] <<0) | (temp[13]<<8) | (temp[2] <<16)  | (temp[7] << 24);
+    state[3] = (temp[12] <<0)| (temp[1] <<8) | (temp[6] <<16)  | (temp[11]<< 24);
+}
+void SBox(uint8_t byte_in[4])
+{   
+    static const uint8_t sbox[256] = {
+        0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76, 
+        0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0, 
+        0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+        0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75, 
+        0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84, 
+        0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+        0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8, 
+        0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2, 
+        0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73, 
+        0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb, 
+        0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79, 
+        0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08, 
+        0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+        0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+        0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf, 
+        0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16  
+    };
+
+    for (int i=0; i<4 ; i++){
+        byte_in[i] = sbox[byte_in[i]];
+    }
+} 
+void SubBytes(uint32_t state[4])
+{
+    uint8_t temp[4];
+    for (int i=0; i<4; i++){
+        temp[0] = (state[i] >> 0) & 0xFF;
+        temp[1] = (state[i] >> 8) & 0xFF;
+        temp[2] = (state[i] >> 16) & 0xFF;
+        temp[3] = (state[i] >> 24) & 0xFF;
+
+        SBox(temp);
+        
+        state[i] = ((uint32_t) temp[0] << 0) | ((uint32_t)temp[1] << 8) | ((uint32_t)temp[2] << 16) | ((uint32_t)temp[3] << 24);
+        
+    }
+}
+void KeyExpansion(const uint32_t key_in[4], uint32_t key_out[44]) 
+{
+    uint32_t temp[4];
+
+    for (int i=0; i<4; i++){
+        key_out[i] = key_in[i];
+    }
+
+    for (int i=4; i<44; i++){
+        temp[0] = key_out[i-1] >> 0  & 0xFF;
+        temp[1] = key_out[i-1] >> 8  & 0xFF;
+        temp[2] = key_out[i-1] >> 16 & 0xFF;
+        temp[3] = key_out[i-1] >> 24 & 0xFF;
+
+        if (i % 4 == 0){
+            RotWord(temp, temp);
+            SBox(temp);
+
+            if((i/4) <= 10) {
+                temp[0] ^= rcon[i/4];
+            }
+        }
+
+        uint32_t temp_word =   ((uint32_t)temp[0] << 0) | ((uint32_t)temp[1] << 8) | ((uint32_t)temp[2] << 16)| ((uint32_t)temp[3] << 24);
+        key_out[i]    =   key_out[i-4] ^ temp_word;
+    }
+}
+void AES128_Encrypt(const uint32_t plaintext[4], const uint32_t key[4], uint32_t ciphertext[4])
+{
+    uint32_t state[4];
+    uint32_t roundKeys[44];
+
+    KeyExpansion(key, roundKeys);
+
+    AddRoundKey(plaintext, &roundKeys[0], state);
+
+    for (int round=1; round <=9; round++){
+        SubBytes    (state);
+        ShiftRows   (state);
+        MixColumns  (state);
+        AddRoundKey (state, &roundKeys[round*4], state);
+    }
+
+    SubBytes    (state);
+    ShiftRows   (state);
+    AddRoundKey (state, &roundKeys[40], ciphertext);
+}
