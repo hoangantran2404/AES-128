6a6ebd27af562d0b9863b30af5cf6f5fa69a1901
Ngo Tran Hoang An <ngotranhoangan2007@gmail.com>
2025-12-13 17:21:54 -0600

    Upload module AES_top, MP_in and MP_out

---
+++ RTL/AES128_top.v
@@ -0,0 +1,98 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/13/2025 03:50:08 PM
+// Design Name: 
+// Module Name: AES128_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module AES128_top(
+    input wire clk, 
+    input wire rst_n,
+    input wire data_in,
+
+    output wire data_out
+);
+    //==================================================//
+    //                      WIRE                        //
+    //==================================================//
+    wire        UART_dv_out_w;
+    wire        MP_in_dv_out_w;
+    wire        core_dv_out_w;
+    wire        MP_out_dv_out_w;
+    wire        TX_done_w;
+    wire        TX_active_w;
+    wire [7:0]  RX_data_out_w;
+    wire [7:0]  MP_out_data_out_w;
+    wire [31:0] MP_in_data_out_w;
+    wire [31:0] core_data_out_w;
+    
+    
+    //==================================================//
+    //                  Instantiate                     //
+    //==================================================//
+
+    receiver UART_RX(
+        .CLK                (clk            ),
+        .Rx_Serial_in       (data_in        ),
+
+        .Rx_DV_out          (UART_dv_out_w  ),
+        .Rx_Byte_out        (RX_data_out_w  )
+    );
+
+    MP_in module_Message_Packer_in(
+        .clk            (clk                ),
+        .rst_n          (rst_n              ),
+        .RX_DV_in       (UART_dv_out_w      ),
+        .uart_byte_in   (RX_data_out_w      ),
+
+        .MP_data_out    (MP_in_data_out_w   ),
+        .MP_dv_out      (MP_in_dv_out_w     )
+    );
+
+    AES128_core module_AES128_core(
+        .clk            (clk                ),
+        .rst_n          (rst_n              ),
+        .plaintext_in   (MP_in_data_out_w   ),
+        .MP_dv_in       (MP_in_dv_out_w     ),
+
+        .data_out       (core_data_out_w    ),
+        .core_dv_out    (core_dv_out_w      )
+    );
+
+    MP_out module_Message_Packer_out(
+        .clk            (clk                ),
+        .rst_n          (rst_n              ),
+        .RX_DV_in       (core_dv_out_w      ),
+        .core_byte_in   (core_data_out_w    ),
+        .TX_active_in   (TX_active_w        ),
+        .TX_done_in     (TX_done_w          ),
+
+        .MP_data_out    (MP_out_data_out_w  ),
+        .MP_dv_out      (MP_out_dv_out_w    )
+    );
+
+    transmitter UART_TX(
+        .CLK            (clk                ),
+        .Tx_DV_in       (MP_out_dv_out_w    ),
+        .Tx_Byte_in     (MP_out_data_out_w  ),
+
+        .Tx_Active_out  (TX_active_w        ),
+        .Tx_Done_out    (TX_done_w          ),
+        .Tx_Serial_out  (data_out           )
+    );
+endmodule
---
+++ RTL/MP_in.v
@@ -0,0 +1,157 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/13/2025 03:48:46 PM
+// Design Name: 
+// Module Name: MP_in
+// Project Name: AES128
+// Target Devices: ZCU102
+// Tool Versions: 
+// Description: Message Packer receives data from UART RX 8-bit/1 cycle clock and send data to core 32-bit/clock.
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MP_in #(
+
+    parameter DATA_WIDTH    = 32
+)(
+    input  wire                             clk,    
+    input  wire                             rst_n,  
+    input  wire [7:0]                       uart_byte_in,     
+    input  wire                             RX_DV_in,
+
+    output wire [DATA_WIDTH-1 :0]           MP_data_out,
+    output wire                             MP_dv_out     
+);
+
+    //==================================================//
+    //                 State Encoding                   //
+    //==================================================//
+    reg  [2:0] current_state_r;
+    reg  [2:0] next_state_r;
+
+    localparam s_PRELOAD      = 3'b000;
+    localparam s_RX_DATA_BITS = 3'b001; 
+    localparam s_SEND         = 3'b010;
+    localparam s_CLEANUP      = 3'b011;
+
+    //==================================================//
+    //                   Registers                      //
+    //==================================================//
+    reg [4:0]                     MP_count_r;
+    reg [127:0]                   plaintext_address_r ;  
+    reg [127:0]                   key_address_r;
+
+    wire                          RX_done_flag_w; 
+    wire                          SEND_done_flag_w;    
+
+    //==================================================//
+    //             Combinational Logic                  //
+    //==================================================//
+
+    assign MP_dv_out        = (current_state_r == s_SEND);
+
+    assign RX_done_flag_w   = (current_state_r == s_RX_DATA_BITS) && (MP_count_r == 5'd32) ; 
+    assign SEND_done_flag_w = (current_state_r == s_SEND && MP_count_r == 5'd7); 
+
+    assign MP_data_out         = (current_state_r == s_SEND ) ?  ((MP_count_r <5'd16)? plaintext_address_r[127-32*MP_count_r -:32]: key_address_r[127-32*MP_count_r -:32]) : 32'd0;
+
+    //==================================================//
+    //                  Next State Logic                //
+    //==================================================//
+    always @(current_state_r or RX_DV_in or RX_done_flag_w or SEND_done_flag_w) begin
+        case(current_state_r)
+            s_PRELOAD: 
+                if(RX_DV_in) 
+                    next_state_r = s_RX_DATA_BITS;
+                else          
+                    next_state_r = s_PRELOAD;
+            
+            s_RX_DATA_BITS: 
+                if(RX_done_flag_w) 
+                    next_state_r = s_SEND; 
+                else               
+                    next_state_r = s_RX_DATA_BITS;
+        
+            s_SEND: 
+                if (SEND_done_flag_w) 
+                    next_state_r = s_CLEANUP;    
+                else                  
+                    next_state_r = s_SEND;
+            
+            s_CLEANUP: 
+                next_state_r = s_PRELOAD;
+            
+            default: 
+                next_state_r = s_PRELOAD;
+        endcase
+    end
+
+    //==================================================//
+    //                State Register (FSM)              //
+    //==================================================//
+    always @(posedge clk or negedge rst_n ) begin
+        if(!rst_n) 
+            current_state_r <= s_PRELOAD;
+        else       
+            current_state_r <= next_state_r;
+    end
+
+    //==================================================//
+    //                   Datapath                       //
+    //==================================================//
+    integer i;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            MP_count_r <= 0;
+            plaintext_address_r  <= 128'd0;
+            key_address_r        <= 128'd0;
+        end else begin
+            if (current_state_r != next_state_r) begin
+                if (next_state_r == s_SEND)      
+                    MP_count_r <= 0; 
+                else if (next_state_r == s_PRELOAD) 
+                    MP_count_r <= 0;
+            end
+
+            case(current_state_r)
+                s_PRELOAD: begin
+                   if (RX_DV_in) begin
+                        plaintext_address_r[127 -: 8] <= uart_byte_in;
+                        MP_count_r                    <= 1;
+                    end
+                end
+
+                s_RX_DATA_BITS: begin
+                    if(RX_DV_in)
+                        if(MP_count_r < 5'd16) begin
+                            plaintext_address_r[127 - 8*MP_count_r -:8] <= uart_byte_in;             
+                            MP_count_r                                  <= MP_count_r + 1;
+                        end else begin
+                            key_address_r[127 -8*MP_count_r -:8] <= uart_byte_in;
+                            MP_count_r                           <= MP_count_r + 1;
+                        end
+
+                end
+
+                s_SEND: begin
+                    if(MP_count_r < 5'd8)
+                        MP_count_r   <= MP_count_r + 1;        
+                end
+
+                s_CLEANUP: begin                         
+                end
+            endcase
+        end
+    end
+
+endmodule
