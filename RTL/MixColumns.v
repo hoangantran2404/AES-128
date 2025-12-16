2dd372ad46736b2310c5edec076b8fb279b498c4
Ngo Tran Hoang An <ngotranhoangan2007@gmail.com>
2025-12-16 15:56:56 -0600

    Refactor MixColumns and GFMult modules
    Refactored MixColumns module to use Matrix_Multiplication for processing inputs and simplified GFMult implementation.

--- RTL/MixColumns.v
+++ RTL/MixColumns.v
@@ -18,8 +18,6 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module MixColumns #(
     parameter DATA_WIDTH = 32
 )
@@ -29,86 +27,53 @@ module MixColumns #(
     input wire [DATA_WIDTH-1:0] in_mc2,
     input wire [DATA_WIDTH-1:0] in_mc3,
 
-    output wire [DATA_WIDTH-1:0] out_mc0, //Accord to column
+    output wire [DATA_WIDTH-1:0] out_mc0, 
     output wire [DATA_WIDTH-1:0] out_mc1,
     output wire [DATA_WIDTH-1:0] out_mc2,
     output wire [DATA_WIDTH-1:0] out_mc3
 );
-    // State 0
-    wire [7:0] s00 = in_mc0[31:24];
-    wire [7:0] s01 = in_mc0[23:16];
-    wire [7:0] s02 = in_mc0[15:8] ;
-    wire [7:0] s03 = in_mc0[7:0]  ;
+    Matrix_Multiplication MM0(.data_in(in_mc0),.data_out(out_mc0));
+    Matrix_Multiplication MM1(.data_in(in_mc1),.data_out(out_mc1));
+    Matrix_Multiplication MM2(.data_in(in_mc2),.data_out(out_mc2));
+    Matrix_Multiplication MM3(.data_in(in_mc3),.data_out(out_mc3));
 
-    // State 1
-    wire [7:0] s10 = in_mc1[31:24];
-    wire [7:0] s11 = in_mc1[23:16];
-    wire [7:0] s12 = in_mc1[15:8] ;
-    wire [7:0] s13 = in_mc1[7:0]  ;
+endmodule
 
-    // State 2
-    wire [7:0] s20 = in_mc2[31:24];
-    wire [7:0] s21 = in_mc2[23:16];
-    wire [7:0] s22 = in_mc2[15:8] ;
-    wire [7:0] s23 = in_mc2[7:0]  ;
+module Matrix_Multiplication #(
+    parameter DATA_WIDTH =32
+)
+(
+    input wire  [DATA_WIDTH-1:0] data_in,
+    output wire [DATA_WIDTH-1:0] data_out
+);
+    wire [7:0] s0 = data_in[31:24];
+    wire [7:0] s1 = data_in[23:16];
+    wire [7:0] s2 = data_in[15:8];
+    wire [7:0] s3 = data_in[7:0];
 
-    // State 3
-    wire [7:0] s30 = in_mc3[31:24];
-    wire [7:0] s31 = in_mc3[23:16];
-    wire [7:0] s32 = in_mc3[15:8] ;
-    wire [7:0] s33 = in_mc3[7:0]  ;
+    wire [7:0] s0x2, s0x3;
+    wire [7:0] s1x2, s1x3;
+    wire [7:0] s2x2, s2x3;
+    wire [7:0] s3x2, s3x3;
 
-    // Output 0
-    wire [7:0] o00 ;
-    wire [7:0] o01 ;
-    wire [7:0] o02 ;
-    wire [7:0] o03 ;
+    GFMult m0x2(.a(s0),.b(4'd2),.p(s0x2));
+    GFMult m0x3(.a(s0),.b(4'd3),.p(s0x3));
 
-    GFMult (.a(s00),.b(4'h02),.p(o00));
-    GFMult (.a(s01),.b(4'h01),.p(o01));
-    GFMult (.a(s02),.b(4'h01),.p(o02));
-    GFMult (.a(s03),.b(4'h03),.p(o03));
+    GFMult m1x2(.a(s1),.b(4'd2),.p(s1x2));
+    GFMult m1x3(.a(s1),.b(4'd3),.p(s1x3));
 
-    assign out_mc0={o00, o01, o02, o03};
-    // Output 1
-    wire [7:0] o10 ;
-    wire [7:0] o11 ;
-    wire [7:0] o12 ;
-    wire [7:0] o13 ;
+    GFMult m2x2(.a(s2),.b(4'd2),.p(s2x2));
+    GFMult m2x3(.a(s2),.b(4'd3),.p(s2x3));
 
-    GFMult (.a(s10),.b(4'h03),.p(o10));
-    GFMult (.a(s11),.b(4'h02),.p(o11));
-    GFMult (.a(s12),.b(4'h01),.p(o12));
-    GFMult (.a(s13),.b(4'h01),.p(o13));
+    GFMult m3x2(.a(s3),.b(4'd2),.p(s3x2));
+    GFMult m3x3(.a(s3),.b(4'd3),.p(s3x3));
 
-    assign out_mc1={o10, o11, o12, o13};
+    wire [7:0] d0 = s0x2 ^ s1x3 ^ s2   ^ s3     ;
+    wire [7:0] d1 = s0   ^ s1x2 ^ s2x3 ^ s3     ;
+    wire [7:0] d2 = s0   ^ s1   ^ s2x2 ^ s3x3   ;
+    wire [7:0] d3 = s0x3 ^ s1   ^ s2   ^ s3x2   ;
 
-    // Output 2
-    wire [7:0] o20 = out_mc2[31:24];
-    wire [7:0] o21 = out_mc2[23:16];
-    wire [7:0] o22 = out_mc2[15:8] ;
-    wire [7:0] o23 = out_mc2[7:0]  ;
-
-    GFMult (.a(s20),.b(4'h01),.p(o20));
-    GFMult (.a(s21),.b(4'h03),.p(o21));
-    GFMult (.a(s22),.b(4'h02),.p(o22));
-    GFMult (.a(s23),.b(4'h01),.p(o23));
-
-    assign out_mc2={o20, o21, o22, o23};
-
-    // Output 3
-    wire [7:0] o30 ;
-    wire [7:0] o31 ;
-    wire [7:0] o32 ;
-    wire [7:0] o33 ;
-
-    GFMult (.a(s30),.b(4'h01),.p(o30));
-    GFMult (.a(s31),.b(4'h01),.p(o31));
-    GFMult (.a(s32),.b(4'h03),.p(o32));
-    GFMult (.a(s33),.b(4'h02),.p(o33));
-
-    assign out_mc3={o30, o31, o32, o33};
-
+    assign data_out = {d0,d1,d2,d3};
 endmodule
 
 module GFMult(
@@ -117,30 +82,32 @@ module GFMult(
 
     output reg [7:0] p
 );
-// This is Galois Field Multiplication (GF) 
-// You can search for the operating rules on Gen AI. 
-// Constants: 8'h1B (0001 1011) is AES reduction modulo. x8 = x4 + x3 + x + 1
-//            8'h80 (1000 0000) is MSB of a.
     reg [7:0] temp_a;
 
-    always @(a or b) begin
-        temp_a     <= a;
-        p          <= 0;
+    always @(a or b) begin 
+        temp_a     = a;
+        p          = 0;
 
         if(b[0]) begin
-            p      <= p ^ temp_a;
-            temp_a <= (temp_a << 1) ^ ( (temp_a & 8'h80) ? 8'h1B : 8'h00);
+            p = p ^ temp_a;
         end
+        
+        temp_a = (temp_a << 1) ^ ( (temp_a & 8'h80) ? 8'h1B : 8'h00);
+
         if(b[1]) begin
-            p      <= p ^ temp_a;
-            temp_a <= (temp_a << 1) ^ ( (temp_a & 8'h80) ? 8'h1B : 8'h00);
+            p = p ^ temp_a;
         end
-        if (b[2])
-            p      <= p ^ temp_a;
-            temp_a <= (temp_a << 1) ^ ( (temp_a & 8'h80) ? 8'h1B : 8'h00);
+
+        temp_a = (temp_a << 1) ^ ( (temp_a & 8'h80) ? 8'h1B : 8'h00);
+
+        if (b[2]) begin
+            p = p ^ temp_a;
+        end
+
+        temp_a = (temp_a << 1) ^ ( (temp_a & 8'h80) ? 8'h1B : 8'h00);
 
         if (b[3]) 
-            p      <= p ^ temp_a;
+            p = p ^ temp_a;
     end
 
 endmodule
